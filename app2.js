4407495378aad11cacf8b2d0d19f1d20730972da
Ankitha-Bade <74085861+Ankitha-Bade@users.noreply.github.com>
2021-02-06 10:54:31 -0700

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,23 @@
+const answer1=document.getElementById("answer1");
+const answer2=document.getElementById("answer2");
+const answer3=document.getElementById("answer3");
+
+const result1=document.getElementById("result1");
+const result2=document.getElementById("result2");
+const result3=document.getElementById("result3");
+
+
+
+answer1.onclick=()=>{
+  
+  result1.style.display="block";
+  next.style.display="block"
+}
+answer2.onclick=()=>{
+  result2.style.display="block";
+  next.style.display="block"
+}
+answer3.onclick=()=>{
+  result3.style.display="block";
+  next.style.display="block"
+}
